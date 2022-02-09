659f8fc60379bf741610e9f2daed9148a1578c4d
Cory Thompson <cory.thompson98@gmail.com>
2022-02-09 12:45:40 -0600

    14.2

--- server.js
+++ server.js
@@ -20,3 +20,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
